f8afc27b17458d4a5bd185b1dab8f5b128e2857b
fdllrs <dellarosafacundo@gmail.com>
2026-07-12 14:53:37 -0300

    stddev test

--- src/test/java/core/ExampleProgramsTest.java
+++ src/test/java/core/ExampleProgramsTest.java
@@ -22,14 +22,14 @@ public class ExampleProgramsTest {
 
 	@Test
 	public void testNormalNormalConjugate() {
-		verifyExample(TestProgram.normalNormalConjugate(), 1.150);
+		verifyExample(TestProgram.normalNormalConjugate(), 1.150, 0.707);
 	}
 
-	private void verifyExample(List<Expression> program, double exactMean) {
-		verifyExample(program, exactMean, TOLERANCE);
+	private void verifyExample(List<Expression> program, double exactMean, double exactStdDev) {
+		verifyExample(program, exactMean, exactStdDev, TOLERANCE);
 	}
 
-	private void verifyExample(List<Expression> program, double exactMean, double tolerance) {
+	private void verifyExample(List<Expression> program, double exactMean, double exactStdDev, double tolerance) {
 		Random random = new Random(42);
 
 		// 1. Likelihood Weighting
@@ -37,7 +37,8 @@ public class ExampleProgramsTest {
 																   new Random(random.nextLong()),
 																   LW_ITERATIONS);
 		Posterior<Number> lwPosterior = lw.run();
-		assertEquals(exactMean, lwPosterior.mean(), tolerance, "LW failed to converge");
+		assertEquals(exactMean, lwPosterior.mean(), tolerance, "LW mean failed to converge");
+		assertEquals(exactStdDev, lwPosterior.stdDev(), tolerance, "LW stdDev failed to converge");
 
 		// 2. Single-Site Metropolis-Hastings
 		SSMetropolisHastings<Number> ssmh = new SSMetropolisHastings<>(program,
@@ -45,63 +46,70 @@ public class ExampleProgramsTest {
 																	   SSMH_WARMUP,
 																	   SSMH_ITERATIONS);
 		Posterior<Number> ssmhPosterior = ssmh.run();
-		assertEquals(exactMean, ssmhPosterior.mean(), tolerance, "SSMH failed to converge");
+		assertEquals(exactMean, ssmhPosterior.mean(), tolerance, "SSMH mean failed to converge");
+		assertEquals(exactStdDev, ssmhPosterior.stdDev(), tolerance, "SSMH stdDev failed to converge");
 
 		// 3. Sequential Monte Carlo
 		SequentialMonteCarlo<Number> smc = new SequentialMonteCarlo<>(program,
 																	  new Random(random.nextLong()),
 																	  SMC_PARTICLES);
 		Posterior<Number> smcPosterior = smc.run();
-		assertEquals(exactMean, smcPosterior.mean(), tolerance, "SMC failed to converge");
+		assertEquals(exactMean, smcPosterior.mean(), tolerance, "SMC mean failed to converge");
+		assertEquals(exactStdDev, smcPosterior.stdDev(), tolerance, "SMC stdDev failed to converge");
 	}
 
 	@Test
 	public void testNoisyBernoulliSum() {
-		verifyExample(TestProgram.noisyBernoulliSum(), 5.014);
+		verifyExample(TestProgram.noisyBernoulliSum(), 5.014, 1.146);
 	}
 
 	@Test
 	public void testMultiObsNormalNormal() {
-		verifyExample(TestProgram.multiObsNormalNormal(), 1.333);
+		verifyExample(TestProgram.multiObsNormalNormal(), 1.333, 0.577);
 	}
 
 	@Test
 	public void testHighVarianceNormalPrior() {
-		verifyExample(TestProgram.highVarianceNormalPrior(), 2.231);
+		verifyExample(TestProgram.highVarianceNormalPrior(), 2.231, 1.664);
 	}
 
 	@Test
 	public void testCoinFlipSelection() {
-		verifyExample(TestProgram.coinFlipSelection(), 0.506);
+		verifyExample(TestProgram.coinFlipSelection(), 0.506, 0.500);
 	}
 
 	@Test
 	public void testSignalNoiseSum() {
-		verifyExample(TestProgram.signalNoiseSum(), 0.599, 0.25);
+		verifyExample(TestProgram.signalNoiseSum(), 0.599, 0.895, 0.25);
 	}
 
 	@Test
 	public void testNoisyBinomial() {
-		verifyExample(TestProgram.noisyBinomial(), 5.014);
+		verifyExample(TestProgram.noisyBinomial(), 5.014, 1.146);
 	}
 
 	@Test
 	public void testExponentialExponentialConjugate() {
-		verifyExample(TestProgram.exponentialExponentialConjugate(), 0.667);
+		verifyExample(TestProgram.exponentialExponentialConjugate(), 0.667, 0.471);
 	}
 
 	@Test
 	public void testUniformNormal() {
-		verifyExample(TestProgram.uniformNormal(), 4.500);
+		verifyExample(TestProgram.uniformNormal(), 4.500, 1.000);
 	}
 
 	@Test
 	public void testBetaBernoulliConjugate() {
-		verifyExample(TestProgram.betaBernoulliConjugate(), 0.600);
+		verifyExample(TestProgram.betaBernoulliConjugate(), 0.600, 0.200);
 	}
 
 	@Test
 	public void testGammaExponentialConjugate() {
-		verifyExample(TestProgram.gammaExponentialConjugate(), 1.200);
+		verifyExample(TestProgram.gammaExponentialConjugate(), 1.200, 0.693);
+	}
+
+	@Test
+	public void testGammaPoissonConjugate() {
+		verifyExample(TestProgram.gammaPoissonConjugate(), 3.333, 1.491);
 	}
 }
